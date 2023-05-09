1b80ecea01ca15b2b358ee9a55778e1005433497
gjj-mapa <cashman91431+gjj@gmail.com>
2023-05-09 20:49:13 +0200

    Small changes

--- js/move man.js
+++ js/move man.js
@@ -1,1 +1,7 @@
-import {meSpace, enemySpaces} from "grid tonk.js";
+import {meSpace, enemySpaces} from "./grid tonk.js";
+const moveButton = document.querySelector(".move-button");
+
+moveButton.addEventListener("click", () => {
+    console.log("move has been pressed")
+})
+
